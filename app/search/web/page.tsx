9e792122a75a492626ce3eb03caeef3b6ef135e1
ryan-didevar <abdidevar@gmail.com>
2023-12-04 17:50:32 +0330

    GoogleSearchEngine API added

--- app/search/web/page.tsx
+++ app/search/web/page.tsx
@@ -3,9 +3,43 @@ interface Props {
     searchTerm: string;
   };
 }
+interface GoogleResult {
+  kind: string;
+  url: {
+    type: string;
+    template: string;
+  };
+  context: { title: string };
+  searchInformation: {
+    searchTime: number;
+    formattedSearchTime: string;
+    totalResults: string;
+    formattedTotalResults: string;
+  };
+  items: {
+    kind: string;
+    title: string;
+    htmlTitle: string;
+    link: string;
+    displayLink: string;
+    snippet: string;
+    htmlSnippet: string;
+    formattedUrl: string;
+    htmlFormattedUrl: string;
+  }[];
+}
 
-const WebSearchPage = ({ searchParams: { searchTerm } }: Props) => {
-  return <div>{searchTerm}</div>;
+const WebSearchPage = async ({ searchParams: { searchTerm } }: Props) => {
+  const url = `https://www.googleapis.com/customsearch/v1?key=${process.env.GO_API_KEY}&cx=${process.env.GO_CONTEXT_KEY}&q=${searchTerm}}`;
+  const res = await fetch(url);
+  const googleResult: GoogleResult = await res.json();
+  return (
+    <div>
+      {googleResult.items.map((item) => (
+        <h1 key={item.link}>{item.title}</h1>
+      ))}
+    </div>
+  );
 };
 
 export default WebSearchPage;
--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   env: {
     API_KEY: process.env.API_KEY,
     LOOKUP_API_KEY: process.env.LOOKUP_API_KEY,
+    GO_API_KEY: process.env.GO_API_KEY,
+    GO_CONTEXT_KEY: process.env.GO_CONTEXT_KEY,
   },
   images: {
     remotePatterns: [
